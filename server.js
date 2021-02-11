bf58ec6276fec15db0ee09e41b63cf2b16f68971
alirezafshar <alireza_afshar_wd@yahoo.com>
2021-02-10 21:15:12 -0500

    added  urlencoded in server page

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(cors());
 
 app.use(express.static("server-assets"));
 app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 app.use("/login", login);
 app.use("/signup", signup);
