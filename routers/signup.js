a842730e57ef8304d6dcb2f988cd28c3f2cfdfea
alirezafshar <alireza_afshar_wd@yahoo.com>
2021-02-11 19:59:44 -0500

    made uniq email for each user

--- routers/signup.js
+++ routers/signup.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
+const mv = require("mv");
 const formidable = require("formidable");
 const bcrypt = require("bcrypt");
 const userInfo = require("../models/userInfoModel");
@@ -10,28 +11,36 @@ router.post("/", (req, res) => {
     const imgprefix = Date.now() + "-";
     const form = new formidable.IncomingForm();
     form.parse(req, (err, fields, file) => {
+
         if (err) {
             return res.status(401).json({ err: err.message })
         }
 
-        bcrypt.hash(fields.password, saltRound, (err, hash) => {
-            new userInfo().save({
-                email: fields.email,
-                password: hash,
-                imgurl: imgprefix + file.image.name
+        userInfo.where({ email: fields.email }).fetch()
+
+            .then(response => {
+                if (response) {
+                    res.status(201).send("exist")
+                } else {
+                    bcrypt.hash(fields.password, saltRound, (err, hash) => {
+                        return new userInfo().save({
+                            email: fields.email,
+                            password: hash,
+                            imgurl: imgprefix + file.image.name
+                        })
+                    })
+
+                    const tempath = file.image.path;
+                    const newpath = path.join(__dirname, "../server-assets/images/" + imgprefix + file.image.name)
+
+                    mv(tempath, newpath, (err) => {
+                        if (err) throw err
+                        res.status(201).send("added");
+                        res.end()
+                    })
+
+                }
             })
-        })
-
-    })
-
-    form.on("fileBegin", (name, file) => {
-        file.path = path.join(__dirname, "../server-assets/images/" + imgprefix + file.name)
-    })
-
-    form.on("end", () => {
-        setTimeout(() => {
-            return res.status(201).send("added")
-        }, 500)
     })
 })
 
